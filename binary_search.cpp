9be5e2fe694ea1cdf53fe905dc47bf99db0bb59b
Anil kumar yadav <83587655+anilyadavup54@users.noreply.github.com>
2023-09-23 15:20:45 +0530

    Update binary_search.cpp
    updated 23/09/23

--- binary_search.cpp
+++ binary_search.cpp
@@ -1,3 +1,5 @@
+#include<iostream>
+using namespace std;
 int  binary_searchArray(int arr[], int n, int key){
    int low=0;
    int high=n-1;
@@ -16,4 +18,12 @@ int  binary_searchArray(int arr[], int n, int key){
     }
    }
    return -1;
+}
+int main()
+{
+    int arr[]={1,2,3,4,5,6,7,8,9,10};
+    int n=sizeof(arr)/sizeof(arr[0]);
+    int find=binary_searchArray(arr,n,7);
+    cout<<find;
+ return 0;
 }
