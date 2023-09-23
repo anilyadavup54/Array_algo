b2fe26095b88a01912fc3fef87a608f4ec022528
Anil kumar yadav <83587655+anilyadavup54@users.noreply.github.com>
2023-09-23 15:41:59 +0530

    Update bubble_sort.cpp

--- bubble_sort.cpp
+++ bubble_sort.cpp
@@ -1,14 +1,25 @@
-void bubble_sort(int arr[], int n)
-{
-    for(int i=0; i<n-1; i++)
-    {
-        for(int j=0; j<n-1-i; j++)
-        {
-            if(arr[j]>arr[j+1]){
-            int temp=arr[j];
-            arr[j]=arr[j+1];
-            arr[j+1]=temp;
+#include<iostream>
+using namespace std;
+void bubble_sort(int arr[], int n){
+    for(int i=0; i<n-1; i++){ // loop passes
+        for(int j=0; j<n-1-i; j++){ // element check
+            if(arr[j]>arr[j+1]){ //compare
+            int temp=arr[j];     //|
+            arr[j]=arr[j+1];    // | swapping elements
+            arr[j+1]=temp;      // |
             }
         }
     }
+}
+display_array(int arr[], int n){
+   for(int i=0; i<n; i++){
+      cout<<arr[i]<<" ";
+   }
+}
+int main(){
+   int arr[]={2,6,3,5,1,8,9,7,4,10}; //unsorted array
+   int n=sizeof(arr)/sizeof(arr[0]);
+   bubble_sort(arr, n); //call bubble sorting funtion
+   display_array(arr,n); //printing array on screen
+ return 0;
 }
